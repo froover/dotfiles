ca1c1afd1c69675956a76ca9aca92f452dc9f14f
Kentaro Yorozu <k-yorozu@tribeat.com>
2019-09-12 13:22:46 +0900

    link.sh修正

--- link.sh
+++ link.sh
@@ -6,12 +6,12 @@ DOT_DIRECTORY="${HOME}/dotfiles"
 
 echo "link home directory dotfiles"
 cd ${DOT_DIRECTORY}
+
 for f in .??*
 do
-    #無視したいファイルやディレクトリ
     [ "$f" = ".git" ] && continue
-    #[ "$f" = ".config" ] && continue
-    ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+
+    ln -snfv "$DOT_DIRECTORY/$f" "$HOME"/"$f"
 done
 
 #echo "link .config directory dotfiles"
